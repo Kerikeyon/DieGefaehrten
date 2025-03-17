c5cbaf6ed7be59b1d38f75b2bf7956a21a3f921b
jpt-dabreu <joaopedrotabreu@gmail.com>
2025-03-17 14:29:29 +0100

    test

--- jumpkinglike.py
+++ jumpkinglike.py
@@ -1,22 +1,30 @@
 import pygame as pygame
 
 pygame.init()
-Weiss  = ( 255, 255, 255)
-x = 50
-y = 50
-width = 40
-height = 60
-vel = 10
+
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode((640,480))
-clock = pygame.time.Clock
 spielaktive = True
+Cx , Cy = 640/2 , 480/2
+dt = 0
+
+class Player:
+    def __init__(self):
+        self.p = pygame.Vector2(Cx - 25, Cy - 25)
+
+player = Player()
+
+
 while spielaktive:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             spielaktive = False
 
-    keys = pygame.key.get_pressed()
-    screen.fill(Weiss)
+    screen.fill('white')
+
+    pygame.draw.rect(screen, 'red', [player.p[0], player.p[1], 50, 50])
+    pygame.draw.rect(screen, 'black', [Cx-100, Cy+25, 200, 50])
+
+    clock.tick(60)
+
     pygame.display.flip()
-    pygame.draw.rect(screen, (255, 0, 0), (x, y, width, height))
-    pygame.display.update()
