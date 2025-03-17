a4574e47f9ebeb7c39022d735241c012926b3de1
Kerikeyon <132877545+Kerikeyon@users.noreply.github.com>
2025-03-17 13:10:26 +0100

    Add files via upload

---
+++ jumpkinglike.py
@@ -0,0 +1,17 @@
+import pygame as pygame
+
+pygame.init()
+Weiss  = ( 255, 255, 255)
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+pygame.display.set_mode((640,480))
+spielaktive = True
+while spielaktive:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            spielaktive = False
+
+    keys = pygame.key.get_pressed()
