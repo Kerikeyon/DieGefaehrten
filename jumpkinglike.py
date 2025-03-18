87b415fbceee715a54fd57b991e53e8eee9682bd
Kerikeyon <pyumamartiny2001@gmail.com>
2025-03-18 12:30:55 +0100

    jump and grav

--- jumpkinglike.py
+++ jumpkinglike.py
@@ -1,4 +1,5 @@
 import pygame as pygame
+import math
 
 #git add .
 #git commit -m "test"
@@ -25,6 +26,7 @@ class Player:
         self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
         self.isjump = False
         self.jumpCount = 10
+        self.gravity = 0.8
 
     def KeyPress(self):
         keys = pygame.key.get_pressed()
@@ -32,32 +34,24 @@ class Player:
             self.x += self.v[0]
         if keys[pygame.K_a]:
             self.x -= self.v[0]
-        if keys[pygame.K_w]:
-            self.y -= self.v[1]
         if keys[pygame.K_s]:
             self.y += self.v[1]
+        if keys[pygame.K_SPACE] and not self.isjump:
+            self.isjump = True
+            self.jumpCount = 10
 
         self.rect.x = self.x
         self.rect.y = self.y
-    def gravity(self):
-        self.y += 3.2
-        if self.rect.y > screen_height and self.y >= 0:
-            self.y = 0
-            self.rect.y = screen_height -self.y -self.y
-    def jump(self):
-        if self.isjump:
-            if self.jumpCount >= -10:
-                neg = 1
-                if self.jumpCount < 0:
-                    neg = -1
-                self.y -= self.jumpCount ** 2 * 0.1 * neg
-                self.jumpCount -= 1
 
-    def gravity(self):
-        self.y += 3.2
-        if self.rect.y > screen_height and self.y >= 0:
-            self.y = 0
-            self.rect.y = screen_height -self.y -self.y
+
+    def applyGravity(self):
+        if not self.isjump:
+            self.v[1] += self.gravity
+        else:
+            self.v[1] = -10
+        self.y += self.v[1]
+        self.rect.y = self.y
+
     def jump(self):
         if self.isjump:
             if self.jumpCount >= -10:
@@ -66,6 +60,17 @@ class Player:
                     neg = -1
                 self.y -= self.jumpCount ** 2 * 0.1 * neg
                 self.jumpCount -= 1
+            else:
+                self.isjump = False
+
+    def resetJump(self):
+        if self.rect.bottom >= screen_height:
+            self.rect.bottom = screen_height
+            self.isjump = False
+            self.jumpCount = 10
+            self.v[1] = 0
+
+
 
 class Platform:
     def __init__(self, farbe, x, y, width, height):
@@ -92,7 +97,8 @@ while spielaktive:
     for platform in list_platform:
         pygame.draw.rect(screen, platform.f, platform.rect)
 
-    player1.gravity()
+    player1.applyGravity()
+    player1.jump()
 
     # Kollisionen
     for platform in list_platform:
@@ -118,8 +124,7 @@ while spielaktive:
                 player1.v[0] = 0
                 player1.v[0] = v0x
 
-    player1.gravity()
-
+    player1.resetJump()
     clock.tick(60)
 
     pygame.display.flip()
