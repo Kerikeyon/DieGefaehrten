3b40a1d79f20792f4363442513a8b5115168e5b9
jpt-dabreu <joaopedrotabreu@gmail.com>
2025-03-21 14:01:18 +0100

    Update Enemy.py

--- Enemy.py
+++ Enemy.py
@@ -1,16 +1,32 @@
 #enemy class
 import pygame
-class Enemy:
-    def __init__(self, farbe, x, y, width, height, texture=None):
+class Gegner:
+    def __init__(self, farbe, x, y, width, height, speed = 3, bewegungsbereich = 200, texture = None):
         self.f = farbe
         self.x = x
         self.y = y
         self.w = width
         self.h = height
-        self.texture = texture
-        self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
+        self.speed = speed  
+        self.start_x = x  
+        self.bewegungsbereich = bewegungsbereich  
+        self.richtung = 1          
+        self.texture = texture        
+        self.rect = pygame.Rect(self.x, self.y, self.w, self.h) 
+
+
+
+    def update(self):
+
+        self.x += self.speed * self.richtung
+        self.rect.x = self.x
+
+        if self.x > self.start_x + self.bewegungsbereich or self.x < self.start_x - self.bewegungsbereich:
+            self.richtung *= -1
+
 
     def draw(self, screen, camera_offset_y):
+
         draw_pos = (self.x, self.y - camera_offset_y)
         if self.texture:
             screen.blit(self.texture, draw_pos)
