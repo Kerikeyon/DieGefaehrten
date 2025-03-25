57643cc9e14fd0292df5f1bb28f5b19ce863afef
PhilippSteinhoff <phsteinhoff@gmx.de>
2025-03-25 17:48:15 +0100

    mehr Texturen und Levelbau

--- jumpkinglike.py
+++ jumpkinglike.py
@@ -50,34 +50,42 @@ def start_game():
     texture_flying_plattform = pygame.image.load(r"Texture/FlyingPlattform.png")
     scaled_image_platform = pygame.transform.scale(texture_flying_plattform, (200, 70))
 
-    texture_Player1 = pygame.image.load(r"Texture\WorrierMain.png")
-    scaled_image_Player1 = pygame.transform.scale(texture_Player1, (52, 52))
+    texture_player1 = pygame.image.load(r"Texture\WorrierMain.png")
+    scaled_image_player1 = pygame.transform.scale(texture_player1, (52, 52))
 
     texture_moving_enemy = pygame.image.load (r"Texture/MovingEnemy.png")
     scaled_image_enemy1 = pygame.transform.scale(texture_moving_enemy,(75,75))
 
-    texture_Player1_Hit = pygame.image.load(r"Texture\WorrierMainBeschädigt.png")
-    scaled_image_Player1_Hit = pygame.transform.scale(texture_Player1_Hit, (52, 52))
+    texture_player1_hit = pygame.image.load(r"Texture\WorrierMainBeschädigt.png")
+    scaled_image_player1_hit = pygame.transform.scale(texture_player1_hit, (52, 52))
+
+    texture_left_beam = pygame.image.load(r"Texture\BalkenLinkeWand.png")
+    scaled_left_beam = pygame.transform.scale(texture_left_beam, (475, 47))
+    texture_right_beam = pygame.image.load(r"Texture\BalkenRechteWand.png")
+    texture_right_short_straight_beam = pygame.image.load(r"Texture/BalkenGeradeRechts.png")
+    scaled_right_straight_short = pygame.transform.scale(texture_right_short_straight_beam, (120, 40))
 
     texture_left_wall = pygame.image.load(r"Texture\LinkeWand.png")
     texture_right_wall = pygame.image.load(r"Texture/RechteWand.png")
 
-    texture_left_beam = pygame.image.load(r"Texture\BalkenLinkeWand.png")
-    texture_right_beam = pygame.image.load(r"Texture\BalkenRechteWand.png")
+    texture_stone_pole_short = pygame.image.load(r"Texture/StonePoleShort.png")
 
     texture_ruby_coin = pygame.image.load (r"Texture\RubyCoin.png")
     scaled_texture_ruby_coin = pygame.transform.scale(texture_ruby_coin, (35, 30))
     texture_gold_coin = pygame.image.load (r"Texture\GoldCoin.png")
     scaled_texture_gold_coin = pygame.transform.scale(texture_gold_coin, (40, 40))
 
-    pygame.mixer.music.load(r"Music\BackgroundMusic1.mp3")
-    pygame.mixer.music.play(loops=-1, start=0.0, fade_ms=0)
+    #pygame.mixer.music.load(r"Music\BackgroundMusic1.mp3")
+    #pygame.mixer.music.play(loops=-1, start=0.0, fade_ms=0)
 
     texture_trophy = pygame.image.load(r"Texture\Trophy.png")
     scaled_texture_trophy = pygame.transform.scale(texture_trophy, (70, 70))
 
     texture_heart = pygame.image.load(r"Texture/Heart.png")
     scaled_texture_heart = pygame.transform.scale(texture_heart, (45, 35))
+
+    texture_double_jump = pygame.image.load(r"Texture/DoubleJumpIcon.png")
+    scaled_double_jump_icon = pygame.transform.scale(texture_double_jump, (50, 50))
 
     bg1 = pygame.image.load (r"Texture\DarkCaveBackground.png")
     bg3 = pygame.image.load(r"Texture\LandscapeBackground.png")
@@ -104,60 +112,71 @@ def start_game():
     list_platform = [
     Platform(BLACK, -20, 570, 520, 500, texture=scaled_texture_floor1),
     Platform(BLACK, 500, 570, 520, 500, texture=scaled_texture_floor1),
+
     Platform(WHITE, 640, 400, 200, 30, texture=scaled_image_platform),
-    Platform(WHITE, 440, 10, 200, 30, texture=scaled_image_platform),
-    Platform(WHITE, 70, -200, 200, 30, texture=scaled_image_platform),
-    Platform(WHITE, 13, 203, 451, 47, texture=texture_left_beam),
+    Platform(WHITE, 640, 10, 200, 30, texture=scaled_image_platform),
+    Platform(WHITE, 100, -100, 200, 30, texture=scaled_image_platform),
+    Platform(WHITE, 730, -800, 200, 30, texture=scaled_image_platform),
+
+    Platform(WHITE, 16, 203, 451, 47, texture=texture_left_beam),
     Platform(WHITE, 538, -309, 451, 47, texture=texture_right_beam),
+    Platform(WHITE, 13,-396,460, 47, texture=scaled_left_beam),
+    Platform(WHITE,340,-768,120,50,texture=scaled_right_straight_short),
+
     Platform(BLACK, 0, -30, 40, 700, texture=texture_left_wall),
     Platform(BLACK, 0, -630, 40, 700, texture=texture_left_wall),
+    Platform(BLACK, 0, -1230, 40, 700, texture=texture_left_wall),
     Platform(BLACK, 1000-53, -30, 40, 700, texture=texture_right_wall),
     Platform(BLACK, 1000-53, -630, 40, 700, texture=texture_right_wall),
+    Platform(BLACK, 1000-53, -1230, 40, 700, texture=texture_right_wall),
+
+    Platform(BLACK,425 ,-717,70,324, texture=texture_stone_pole_short),
     ]
 
     #Liste Rutschen
     list_slope = [
-            Slope(RED, 200, 200, 100, "left")
+            Slope(RED, 445, -770, 50, "right")
 
     ]
+    #Liste Gegner
+    list_enemy = [Enemy(BLUE, 200, 100, 75, 75, 4, texture=scaled_image_enemy1),
+                  Enemy(BLUE, 700, -700,75,75,4, texture=scaled_image_enemy1)
+                  ]
 
-    list_enemy = [Enemy(BLUE, 200, 100, 75, 75, 4, texture=scaled_image_enemy1)]
 
-
-    #Origin Liste Coins
+    #Liste Coins
     origin_ruby_coins = [
-        (RED, 525, -30, 30, 30, scaled_texture_ruby_coin),
+        (RED, 723, -29, 30, 30, scaled_texture_ruby_coin),
+        (RED,183, -136,30,30 ,scaled_texture_ruby_coin),
     ]
-
     origin_gold_coins = [
-        (YELLOW, 300, 160, 30, 30, scaled_texture_gold_coin)
+        (YELLOW, 200, -440, 30, 30, scaled_texture_gold_coin)
     ]
-
     def create_ruby_coins():
         return [Coin(color, x, y, width, height, texture=texture)
                 for color, x, y, width, height, texture in origin_ruby_coins]
     def create_gold_coins():
         return [Coin(color, x, y, width, height, texture=texture)
                 for color, x, y, width, height, texture in origin_gold_coins]
-
     list_ruby_coins = create_ruby_coins()
     list_gold_coins = create_gold_coins()
 
-
-    #Liste leben
+    #Liste Leben
     list_hearts = [Heart(YELLOW, Cx -75, 20, 30, 30, scaled_texture_heart),
                    Heart(YELLOW, Cx -25, 20, 30, 30, scaled_texture_heart),
                    Heart(YELLOW, Cx +25, 20, 30, 30, scaled_texture_heart)]
 
+    list_powerups = [PowerUp(410, -200, 40, 40, texture=scaled_double_jump_icon),
+                     PowerUp(670, -600, 40, 40, texture=scaled_double_jump_icon),
+
+    ]
+
     # Ziel erstellen
-    goal1 = Goal(WHITE, 700, -380, 60, 80,texture=scaled_texture_trophy)
-
+    goal1 = Goal(WHITE, 700, -2380, 60, 80,texture=scaled_texture_trophy)
+    # Deathzone erstellen
     deathzone1 = Deathzone(WHITE, -1000, 900, 20000, 20000)
-
-    powerup = PowerUp(500, 300, 30, 30)
-
-    # create player
-    player1 = Player(RED, Cx - 25, 450, 50, 50, texture=scaled_image_Player1)
+    # Spieler erstellen
+    player1 = Player(RED, Cx - 25, 450, 50, 50, texture=scaled_image_player1)
 
     # Hauptspielschleife
     gameactive = True
@@ -182,24 +201,26 @@ def start_game():
         for enemy in list_enemy:
             enemy.update()
             enemy.draw(screen, camera_offset_y)
-
         # Plattformen zeichnen
         for platform in list_platform:
             platform.draw(screen, camera_offset_y)
-
         #Rutsche zeichnen
         for slope in list_slope:
             slope.draw(screen, camera_offset_y)
-        
         # Coins zeichnen
         for coin in list_ruby_coins:
             coin.draw(screen, camera_offset_y)
+
         for coin in list_gold_coins:
             coin.draw(screen, camera_offset_y)
 
+        for powerup in list_powerups:
+            powerup.check_collision(player1)
+            powerup.update()
+            powerup.draw(screen, camera_offset_y)
+
         # Goal zeichnen
         goal1.draw(screen, camera_offset_y)
-
         #Deathzone Zeichnen
         deathzone1.draw(screen, camera_offset_y)
         # Spieler zeichnen
@@ -214,7 +235,7 @@ def start_game():
         player1.resetJump()
         powerup.check_collision(player1)
         powerup.update()
-        powerup.draw(screen, camera_offset_y)
+
 
         # Coin-Kollision
         for i in range(len(list_ruby_coins)-1, -1, -1):
@@ -243,27 +264,27 @@ def start_game():
                     pygame.mixer.Sound.play(damage)
 
         if invincible:
-            player1.texture = scaled_image_Player1_Hit
+            player1.texture = scaled_image_player1_hit
             player1.f = ORANGE
             current_time = pygame.time.get_ticks()
             elapsed_time = current_time - hitmoment
 
             if (elapsed_time // 50) % 2 == 0:
-                player1.texture = scaled_image_Player1_Hit
+                player1.texture = scaled_image_player1_hit
             else:
-                player1.texture = scaled_image_Player1
+                player1.texture = scaled_image_player1
             if current_time - hitmoment >= 1000:
-                player1.texture = scaled_image_Player1
+                player1.texture = scaled_image_player1
                 invincible = False
                 player1.f = RED
-                player1.texture = scaled_image_Player1
+                player1.texture = scaled_image_player1
 
         # Goal Kollision
         reset_triggered = False
         if player1.rect.colliderect(goal1.rect):
             if not reset_triggered:
                 reset_triggered = True  # Reset nur einmal auslösen
-                player1 = Player(RED, Cx - 25, 450, 50, 50, texture=scaled_image_Player1)
+                player1 = Player(RED, Cx - 25, 450, 50, 50, texture=scaled_image_player1)
                 list_ruby_coins[:] = create_ruby_coins()
                 list_gold_coins[:] = create_gold_coins()
                 score = 0
@@ -278,7 +299,7 @@ def start_game():
         if player1.rect.colliderect(deathzone1.rect):
             if not reset_triggered:
                 reset_triggered = True  # Reset nur einmal auslösen
-                player1 = Player(RED, Cx - 25, 450, 50, 50, texture=scaled_image_Player1)
+                player1 = Player(RED, Cx - 25, 450, 50, 50, texture=scaled_image_player1)
                 list_ruby_coins[:] = create_ruby_coins()
                 list_gold_coins[:] = create_gold_coins()
                 score = 0
@@ -293,7 +314,7 @@ def start_game():
             score = 0
             list_ruby_coins[:] = create_ruby_coins()
             list_gold_coins[:] = create_gold_coins()
-            player1 = Player(RED, Cx - 25, 450, 50, 50, texture=scaled_image_Player1)
+            player1 = Player(RED, Cx - 25, 450, 50, 50, texture=scaled_image_player1)
 
         # Scoreanzeige zeichnen
         score_text = font.render('Score = ' + str(score), True, YELLOW)
